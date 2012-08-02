3bdcd16d99e183ad96b3c9cae503f21b344119ee
Christopher Vollick <psycotica0@gmail.com>
2012-08-01 22:47:38 -0400

    Done Insert
    I think...
    It fits into my head as being cool, and seems to do what I want, but I
    obviously don't know it's good yet.
    
    I've done, like, one test.
    That test seemed to do what I wanted, though...

--- Data/PkTree.hs
+++ Data/PkTree.hs
@@ -3,6 +3,7 @@ import Data.Tree
 import Data.Zone
 import Data.Divisible
 import Data.Betweenable
+import Data.Offsetable
 import qualified Data.List as List -- List.find
 
 data PkNode zone payload = Instantiable {pk_zone :: (Zone zone)} | NonInstantiable {pk_zone :: (Zone zone)} | Leaf {pk_zone :: (Zone zone), payload :: payload} deriving (Show, Eq)
@@ -16,10 +17,10 @@ node_zone = pk_zone.rootLabel
 empty_tree :: (Zone a) -> PkTree a b
 empty_tree zone = Node (NonInstantiable zone) []
 
-insert :: (Divisible point, Betweenable point, Eq point) => (PkTree point payload) -> (PkConfiguration point) -> point -> payload -> (PkTree point payload)
+insert :: (Divisible point, Betweenable point, Eq point, Offsetable point) => (PkTree point payload) -> (PkConfiguration point) -> point -> payload -> (PkTree point payload)
 insert tree config zone payload = insert_zone tree config (Zone zone zone) payload
 
-insert_zone :: (Divisible zone, Betweenable zone, Eq zone) => (PkTree zone payload) -> (PkConfiguration zone) -> (Zone zone) -> payload -> (PkTree zone payload)
+insert_zone :: (Divisible zone, Betweenable zone, Eq zone, Offsetable zone) => (PkTree zone payload) -> (PkConfiguration zone) -> (Zone zone) -> payload -> (PkTree zone payload)
 insert_zone tree config zone payload = insert_node tree config (Node (Leaf zone payload) [])
 
 -- This function returns the input node as the only item in the list if it's instantiable, or its children if it's not.
@@ -34,19 +35,23 @@ check_instantiable node = [node]
 -- If none of the children contain it, then it returns the children as is with the input node as one of them
 -- Also, it takes a function that goes from (a -> [a]) to run on the result of insert_node.
 -- That can be check_instantiable if you want to make sure that the result is instantiable, or return if you don't
-insert_into_children :: (Divisible zone, Betweenable zone, Eq zone) => ((PkTree zone payload) -> [(PkTree zone payload)]) -> (PkConfiguration zone) -> [(PkTree zone payload)] -> (PkTree zone payload) -> [(PkTree zone payload)]
+insert_into_children :: (Divisible zone, Betweenable zone, Eq zone, Offsetable zone) => ((PkTree zone payload) -> [(PkTree zone payload)]) -> (PkConfiguration zone) -> [(PkTree zone payload)] -> (PkTree zone payload) -> [(PkTree zone payload)]
 insert_into_children func config children input = insert_or_not $ List.find (\child -> subset (node_zone child) (node_zone input)) children
 	where
 	insert_or_not Nothing = input:children
 	insert_or_not (Just child) = (func $ insert_node child config input) ++ filter (\i -> (node_zone i) /= (node_zone child)) children
 
 
-insert_node :: (Divisible zone, Betweenable zone, Eq zone) => (PkTree zone payload) -> (PkConfiguration zone) -> (PkTree zone payload) -> (PkTree zone payload)
+insert_node :: (Divisible zone, Betweenable zone, Eq zone, Offsetable zone) => (PkTree zone payload) -> (PkConfiguration zone) -> (PkTree zone payload) -> (PkTree zone payload)
 insert_node (Node pknode children) config node = are_we_done $ insert_into_children check_instantiable config children node
 	where
 	are_we_done children | (length children) < (config_k config) = Node (NonInstantiable (pk_zone pknode)) children
-	are_we_done children = check_subdivisions children
-	check_subdivisions children = Node (Instantiable (pk_zone pknode)) children
+	are_we_done children = what_are_we $ check_subdivisions children
+	check_subdivisions children = concatMap check_instantiable $ populate_subdivisions children
+	populate_subdivisions = foldr (\child subdivisions -> insert_into_children return config subdivisions child) $ map build_node_from_subdivision $ (pk_zone pknode) `divide_zone_by` (config_r config)
+	build_node_from_subdivision zone = Node (NonInstantiable zone) []
+	what_are_we children | (length children) < (config_k config) = Node (NonInstantiable (pk_zone pknode)) children
+	what_are_we children = Node (Instantiable (pk_zone pknode)) children
 
 ---- If it's contained by one of my children, then recursively insert it there
 ---- Elseif it's instantiable, insert it as one of my children
