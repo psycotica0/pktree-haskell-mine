f616fdfbb3e60697ffe32207757ddb541176aa7d
Christopher Vollick <psycotica0@gmail.com>
2013-02-04 13:24:43 -0500

    Implemented Intersection
    I had to rework Betweenable.
    
    All of the functions it provided still exist, and can be written using
    the new methods Betweenable provides, but I had to move Betweenable back
    a level, because it was abstracting too much away.
    
    With the new level, which returns which dimensions overlap, I can write
    the intersect function abstractly for any Betweenable.
    
    The name Betweenable doesn't really make sense anymore, but... meh.
    Maybe I'll change that...

--- Data/Betweenable.hs
+++ Data/Betweenable.hs
@@ -1,30 +1,60 @@
 module Data.Betweenable where
 
--- Like Ord, but more generic I guess, determines if one value is between two others
+import Data.Set (Set, union, empty, singleton, mapMonotonic, size)
+import Data.Bool.HT (if')
+
+-- This one doesn't work for all Ord, only single dimensional instances of Ord
+ord_overlap :: (Ord a) => a -> a -> a -> Set Int
+ord_overlap a b c = if' (and [a <= b, b < c]) (singleton 0) empty
+
+ord_overlap_inclusive :: (Ord a) => a -> a -> a -> Set Int
+ord_overlap_inclusive a b c = if' (and [a <= b, b <= c]) (singleton 0) empty
+
+ord_overlap_exclusive :: (Ord a) => a -> a -> a -> Set Int
+ord_overlap_exclusive a b c = if' (and [a < b, b < c]) (singleton 0) empty
+
+-- Returns true if first <= second < third
+within :: (Betweenable a) => a -> a -> a -> Bool
+within a b c = (==) (dimensions a) $ size $ overlap a b c
+
+-- Returns true if first <= second <= third
+contained_by :: (Betweenable a) => a -> a -> a -> Bool
+contained_by a b c = (==) (dimensions a) $ size $ overlap_inclusive a b c
+
+-- This is the new one I need to handle both subsets and intersections
 class Betweenable a where
-	-- Returns true if first < second < third
-	between :: a -> a -> a -> Bool
-	-- Returns true if first <= second < third
-	within :: a -> a -> a -> Bool
-	-- Returns true if first <= second <= third
-	contained_by :: a -> a -> a -> Bool
+	-- This returns the number of dimensions this datatype represents
+	-- This is sort of a hack, I'm assuming in a few places that for any given a, there is exactly one return value
+	dimensions :: a -> Int
+	-- This function returns a set containing the dimenions (between 0 and the result of `dimensions`) that overlaps in the second value in the range between the first and third
+	-- In this one has an exclusive upper bound, but an inclusive lower bound
+	overlap :: a -> a -> a -> Set Int
+	-- This does the same as the above, but has an inclusive upper bound as well
+	overlap_inclusive :: a -> a -> a -> Set Int
+	-- This does the same, but has an exclusive lower and upper bound
+	overlap_exclusive :: a -> a -> a -> Set Int
 
 instance Betweenable Double where
-	between a b c = (a < b) && (b < c)
-	within a b c = (a <= b) && (b < c)
-	contained_by a b c = (a <= b) && (b <= c)
+	dimensions _ = 1
+	overlap = ord_overlap
+	overlap_inclusive = ord_overlap_inclusive
+	overlap_exclusive = ord_overlap_exclusive
 
 instance Betweenable Integer where
-	between a b c = (a < b) && (b < c)
-	within a b c = (a <= b) && (b < c)
-	contained_by a b c = (a <= b) && (b <= c)
+	dimensions _ = 1
+	overlap = ord_overlap
+	overlap_inclusive = ord_overlap_inclusive
+	overlap_exclusive = ord_overlap_exclusive
 
 instance Betweenable Int where
-	between a b c = (a < b) && (b < c)
-	within a b c = (a <= b) && (b < c)
-	contained_by a b c = (a <= b) && (b <= c)
+	dimensions _ = 1
+	overlap = ord_overlap
+	overlap_inclusive = ord_overlap_inclusive
+	overlap_exclusive = ord_overlap_exclusive
 
 instance (Betweenable a, Betweenable b) => Betweenable (a, b) where
-	between (a, b) (c, d) (e, f) = (between a c e) && (between b d f)
-	within (a, b) (c, d) (e, f) = (within a c e) && (within b d f)
-	contained_by (a, b) (c, d) (e, f) = (contained_by a c e) && (contained_by b d f)
+	dimensions _ = 2
+	-- Need to compute overlap in each dimension, making the second dimension independent of the first
+	overlap (a, b) (c, d) (e, f) = union (overlap a c e) $ mapMonotonic (+ dimensions a) $ overlap b d f
+	overlap_inclusive (a, b) (c, d) (e, f) = union (overlap_inclusive a c e) $ mapMonotonic (+ dimensions a) $ overlap_inclusive b d f
+	overlap_exclusive (a, b) (c, d) (e, f) = union (overlap_exclusive a c e) $ mapMonotonic (+ dimensions a) $ overlap_exclusive b d f
--- Data/Zone.hs
+++ Data/Zone.hs
@@ -3,6 +3,8 @@ import Data.Divisible
 import Data.Offsetable
 import Data.Betweenable
 
+import Data.Set (size, unions)
+
 data Zone a = Zone {lower_bound :: a, upper_bound :: a} deriving (Show, Read, Eq)
 
 divide_zone_by :: (Divisible a, Offsetable a) => Zone a -> a -> [Zone a]
@@ -27,3 +29,13 @@ zone `contains` value = within (lower_bound zone) value (upper_bound zone)
 -- Anyway, this is what I want.
 subset :: (Betweenable a) => (Zone a) -> (Zone a) -> Bool
 big_zone `subset` small_zone = (within (lower_bound big_zone) (lower_bound small_zone) (upper_bound big_zone)) && (contained_by (lower_bound big_zone) (upper_bound small_zone) (upper_bound big_zone))
+
+-- This function returns True if two zones intersect
+-- Two zones are considered to intersect iff every dimension has overlap
+-- In this case, we use overlap_exclusive when comparing high points to ranges, and overlap when comparing low points.
+-- The reason is that we want (overlap low low high) to be true, and (overlap low high high) to be false
+-- That's because zones in this system are in general inclusive in the lower end and exclusive in the upper
+intersect :: (Betweenable a) => (Zone a) -> (Zone a) -> Bool
+intersect (Zone lower1 upper1) (Zone lower2 upper2) = (dimensions lower1) == (size dims)
+	where
+	dims = unions [overlap lower1 lower2 upper1, overlap lower2 lower1 upper2, overlap_exclusive lower1 upper2 upper1, overlap_exclusive lower2 upper1 upper2]
