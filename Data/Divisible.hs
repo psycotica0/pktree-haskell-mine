9f568327a3d4c343dfbfb9ade905354784e8407a
Christopher Vollick <psycotica0@gmail.com>
2013-02-06 15:48:28 -0500

    Reworked Divisible
    I'm not sure I've done the right thing here.
    I think I've made it more complicated somehow...
    
    Anyway, the main upshot is that Divisible only has divide_into_upper and
    divide_into_lower.
    
    Then, I've included a bunch of different constructors for building
    Divisible instances more easily.
    
    I've got ones you can use verbatim for fractional and integral values.
    Then I've got ones that build the upper and lowers out of a generator,
    so long as they're in Num.
    
    The generator does what divide_into used to do, which was return the
    sizes of the blocks.
    Then the above code can take those and derive the block edges.
    
    Those all assume 1D, though.
    
    If none of them work, you can write your own still.

--- Data/Divisible.hs
+++ Data/Divisible.hs
@@ -1,13 +1,41 @@
-module Data.Divisible where
-import Control.Applicative
+module Data.Divisible (
+	Divisible,
+	divide_into_upper, divide_into_lower,
+	upper_from_generator, lower_from_generator,
+	fractional_divide_into_upper, fractional_divide_into_lower,
+	integral_divide_into_upper, integral_divide_into_lower,
+	) where
+
+import Control.Applicative ((<*>), (<$>))
+import Data.Traversable (mapAccumL)
+
+lower_from_generator :: (Num a) => (a -> Int -> [a]) -> a -> Int -> [a]
+lower_from_generator generator a b = take b $ scanl (+) 0 $ generator a b
+
+upper_from_generator :: (Num a) => (a -> Int -> [a]) -> a -> Int -> [a]
+upper_from_generator generator a b = take b $ scanl1 (+) $ generator a b
+
+fractional_generator :: (Fractional a, RealFrac a) => a -> Int -> [a]
+fractional_generator a b = repeat $ a / (fromIntegral b)
+
+fractional_divide_into_lower :: (Fractional a, RealFrac a) => a -> a -> [a]
+fractional_divide_into_lower a b = lower_from_generator fractional_generator a (truncate b)
+
+fractional_divide_into_upper :: (Fractional a, RealFrac a) => a -> a -> [a]
+fractional_divide_into_upper a b = upper_from_generator fractional_generator a (truncate b)
+
+integral_generator :: (Integral a) => a -> Int -> [a]
+integral_generator a b = snd $ mapAccumL iterator a $ reverse [1..(fromIntegral b)]
+	where
+	iterator acc v = let d = div acc v in (acc - d, d)
+
+integral_divide_into_lower :: (Integral a) => a -> a -> [a]
+integral_divide_into_lower a b = lower_from_generator integral_generator a (fromIntegral b)
+
+integral_divide_into_upper :: (Integral a) => a -> a -> [a]
+integral_divide_into_upper a b = upper_from_generator integral_generator a (fromIntegral b)
 
 class Divisible a where
-	-- This just abstracts division so 4 // 3 == 1 and 4.0 // 3.0 == 1.33333
-	(//) :: a -> a -> a
-	-- This divides one number into the other number equal parts.
-	-- The return list is the sizes of each part.
-	-- For example: 8 `divide_into` 4 => [2, 2, 2, 2]
-	divide_into :: a -> a -> [a]
 	-- This spits out a list of "break points" if you divide left side by right
 	-- For example, 8 `divide_into_upper` 4 => [2, 4, 6, 8], representing the 4 "blocks" of 8.
 	-- For example, 8 `divide_into_lower` 4 => [0, 2, 4, 6], as the lower sides of those blocks
@@ -17,28 +45,17 @@ class Divisible a where
 	divide_into_lower :: a -> a -> [a]
 
 instance Divisible Double where
-	(//) = (/)
-	a `divide_into` 0 = []
-	a `divide_into` b = take (truncate b) $ repeat $ a // (fromInteger $ truncate b)
-	a `divide_into_upper` b = tail $ scanl (+) 0 $ a `divide_into` b
-	a `divide_into_lower` b = init $ scanl (+) 0 $ a `divide_into` b
+	divide_into_lower = fractional_divide_into_lower
+	divide_into_upper = fractional_divide_into_upper
 
 instance Divisible Integer where
-	(//) = div
-	a `divide_into` 0 = []
-	a `divide_into` b = (a // b) : (divide_into (a - (a // b)) (b-1))
-	a `divide_into_upper` b = tail $ scanl (+) 0 $ a `divide_into` b
-	a `divide_into_lower` b = init $ scanl (+) 0 $ a `divide_into` b
+	divide_into_lower = integral_divide_into_lower
+	divide_into_upper = integral_divide_into_upper
 
 instance Divisible Int where
-	(//) = div
-	a `divide_into` 0 = []
-	a `divide_into` b = (a // b) : (divide_into (a - (a // b)) (b-1))
-	a `divide_into_upper` b = tail $ scanl (+) 0 $ a `divide_into` b
-	a `divide_into_lower` b = init $ scanl (+) 0 $ a `divide_into` b
+	divide_into_lower = integral_divide_into_lower
+	divide_into_upper = integral_divide_into_upper
 
 instance (Divisible a, Divisible b) => Divisible (a, b) where
-	(a, b) // (c, d) = (a // c, b // d)
-	(a, b) `divide_into` (c, d) = (,) <$> (a `divide_into` c) <*> (b `divide_into` d)
-	(a, b) `divide_into_upper` (c, d) = (,) <$> (a `divide_into_upper` c) <*> (b `divide_into_upper` d)
-	(a, b) `divide_into_lower` (c, d) = (,) <$> (a `divide_into_lower` c) <*> (b `divide_into_lower` d)
+	divide_into_upper (a, b) (c, d) = (,) <$> (divide_into_upper a c) <*> (divide_into_upper b d)
+	divide_into_lower (a, b) (c, d) = (,) <$> (divide_into_lower a c) <*> (divide_into_lower b d)
