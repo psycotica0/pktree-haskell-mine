e71b221ed7ba686a5a3d91f882fe0de77d6471f7
Christopher Vollick <psycotica0@gmail.com>
2013-02-09 13:33:47 -0500

    Renamed Tests
    I want tests to be the automatic coverage tests.
    The other one is just where I was using features to make sure they
    worked like I thought, etc.
    
    So, I've renamed test to example.
    Then I've moved auto_tests to test.

--- auto_tests.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
-import Test.QuickCheck (quickCheck)
-import Data.BetweenableTests as B (test)
-
-main = quickCheck B.test
--- test.hs
+++ test.hs
@@ -1,27 +1,6 @@
-import Data.PkTree (PkTree, empty, insert, lookup_point, move, delete, lookup_zone)
-import Data.Zone (Zone(Zone))
-import Data.Tree (drawTree)
+module Main where
 
-print_tree = putStrLn.drawTree.(fmap show)
+import Test.QuickCheck (quickCheck)
+import Data.BetweenableTests as B (test)
 
-k = 3
-r = (2,2)
-
-main = do
-	putStrLn "Tree:"
-	print_tree result
-	putStrLn "Value at (4,4)"
-	print $ lookup_point (4,4) result
-	putStrLn "Moving (3,3) to (30,30)"
-	print_tree $ move k r (3,3) (30,30) result
-	putStrLn "Deleting (3,3)"
-	print_tree $ delete k r (3,3) result
-	putStrLn "Finding all in zone (4,4) (15,15)"
-	print $ lookup_zone (Zone (4,4) (15,15)) result
-
-root :: PkTree (Int,Int) [Char]
-root = empty (Zone (0,0) (100, 100))
-
-points = [((2,2), "Hello!"), ((3,3), "Yo!"), ((4,4), "Dude!"), ((10, 7), "I'm an outcast!")]
-
-result = foldr (uncurry $ insert k r) root points
+main = quickCheck B.test
